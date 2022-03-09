c4c6cb7adcfb2ae503bc3edc39c8f56bb367accd
ruiined <ruiined@gmail.com>
2022-03-09 15:39:52 +0000

    implemented attack
    Co-authored-by: George Hett <george-h-h@users.noreply.github.com>

--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -13,4 +13,10 @@ feature 'test' do
     sign_in_and_play
     expect(page).to have_content('Noob has 50 health remaining')
   end
+
+  scenario 'attack player 2 and get a confirmation' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content('Pro Gamer attacked Noob')
+  end
 end
