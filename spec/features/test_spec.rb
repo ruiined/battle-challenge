2e34ef369017fdc5ba453c1bb0e3595b6818301f
ruiined <ruiined@gmail.com>
2022-03-09 15:14:31 +0000

    added hit points

--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -11,5 +11,13 @@ feature 'test' do
     click_button('Submit')
     expect(page).to have_content('Pro Gamer vs Noob')
   end
+
+  scenario 'see player 2s hit points' do
+    visit('/')
+    fill_in('player_1', with: 'Pro Gamer')
+    fill_in('player_2', with: 'Noob')
+    click_button('Submit')
+    expect(page).to have_content('Noob has 50 health remaining')
+  end
 end
 
